40a351f16b42b95af2c62930f4a08cd04e93caf1
Maciej Kobus <webhdx@users.noreply.github.com>
2020-07-15 09:56:21 +0200

    EC-190: Added support for SOLR 7.7 (#21)
    * EC-190: Fixed support for SOLR 7.7
    
    * EC-190: Fixed SOLR related env vars

--- install-solr.sh
+++ install-solr.sh
@@ -4,13 +4,13 @@ if [ $# -gt 0 ]; then
 fi
 
 wget https://archive.apache.org/dist/tika/tika-app-1.20.jar -O bin/tika-app-1.20.jar
-wget https://archive.apache.org/dist/lucene/solr/6.6.5/solr-6.6.5.tgz
-tar xfz solr-6.6.5.tgz
-mv solr-6.6.5 solr
+wget https://archive.apache.org/dist/lucene/solr/7.7.3/solr-7.7.3.tgz -O solr-7.7.3.tgz
+tar xfz solr-7.7.3.tgz
+mv solr-7.7.3 solr
 cd solr
 mkdir -p server/ez/template
 cp -R ../migrations/solr/* server/ez/template
-cp server/solr/configsets/basic_configs/conf/{currency.xml,solrconfig.xml,stopwords.txt,synonyms.txt,elevate.xml} server/ez/template
+cp example/example-DIH/solr/solr/conf/{currency.xml,solrconfig.xml,stopwords.txt,synonyms.txt,elevate.xml} server/ez/template
 cp server/solr/solr.xml server/ez
 ## Modify solrconfig.xml to remove section that doesn't agree with our schema
 sed -i.bak '/<updateRequestProcessorChain name="add-unknown-fields-to-the-schema">/,/<\/updateRequestProcessorChain>/d' server/ez/template/solrconfig.xml
